8428b0fa059482fe6694fe172f6ed473ab6f0e61
FransuaLeonSanchez <FransuaLeonSanchez@users.noreply.github.com>
2025-06-21 16:38:33 -0500

    MEJIA-110 feat: incrementar reseñas del blush

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -93,7 +93,7 @@ const products: Product[] = [
     image: "https://images.unsplash.com/photo-1522335789503-aabd1fc54bc9",
     category: "Rostro",
     rating: 4.8,
-    reviews: 95,
+    reviews: 102,
   },
   {
     id: 7,
