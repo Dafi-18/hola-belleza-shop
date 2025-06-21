eb40d60dbe8c4fccb0f63c4f101300a6288abd37
FransuaLeonSanchez <FransuaLeonSanchez@users.noreply.github.com>
2025-06-21 16:06:59 -0500

    MEJIA-38 Agregar emoji al botón limpiar filtros

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -521,7 +521,7 @@ const Index = () => {
                   }}
                   className="border-pink-200 hover:bg-pink-50"
                 >
-                  Limpiar Filtros
+                  🧹 Limpiar Filtros
                 </Button>
               </div>
             </div>
